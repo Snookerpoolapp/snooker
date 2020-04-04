37c2787160228f1f7a9bd1122308e0d42945c01f
Mothusi Molorane <molorane.mothusi@gmail.com>
2020-04-05 00:19:34 +0200

    first commit

---
+++ src/main/java/za/co/jehuco/snooker/SnookerApplication.java
@@ -0,0 +1,13 @@
+package za.co.jehuco.snooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnookerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/jehuco/snooker/SnookerApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.jehuco.snooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
